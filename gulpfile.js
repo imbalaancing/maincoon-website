310513f9bfd1610390f19b743150be35998d6be0
nicky <nikkalaancing@gmail.com>
2022-08-09 00:50:50 +0700

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,98 @@
-const { src, dest } = require ('gulp');
-const { series, parallel } = require('gulp');
+const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const rename = require('gulp-rename');
+const del = require('del');
+const cleanCSS = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
-const browserSync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
+const browsersync = require('browser-sync').create();
+const newer = require('gulp-newer');
 
-function sync() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        },
-        port: 3000
-    });
+const paths = {
+    styles: {
+        src: 'main.scss',
+        dest: 'build/styles/'
+    },
+    html: {
+        src: 'index.html',
+        dest: 'build/'
+    },
+    fonts: {
+        src: 'fonts/**',
+        dest: 'build/fonts/'
+    },
+    images: {
+        src: 'img/**/*',
+        dest: 'build/img/'
+    }
 };
 
-function browserReload() {
-    browserSync.reload();
-}
+function clean() {
+    return del(['build/*', '!build/img']);
+};
+
+function cleanImg() {
+    return del(['build/img/']);
+};
+
+function cleanFonts() {
+    return del(['build/fonts/'])
+};
+
+function copyHtml() {
+    return gulp.src(paths.html.src)
+    .pipe(gulp.dest(paths.html.dest))
+    .pipe(browsersync.stream())
+};
+
+function copyFonts() {
+    return gulp.src(paths.fonts.src)
+    .pipe(gulp.dest(paths.fonts.dest))
+    .pipe(browsersync.stream())
+};
 
 function styles() {
-    return src('./main.scss')
+    return gulp.src(paths.styles.src)
     .pipe(sourcemaps.init())
-    .pipe (sass())
-    .pipe(sass({
-    outputStyle: 'compressed'
+    .pipe(sass())
+    .pipe(cleanCSS())
+    .pipe(rename({
+        basename: 'main',
+        suffix: '.min'
     }))
-    .pipe(sourcemaps.write('./'))
-    .pipe (dest('./css/'))
-    .pipe(browserSync.stream());
-    
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browsersync.stream())
 };
 
-function watchFiles() {
-    gulp.watch('./main.scss', styles);
-    gulp.watch('./index.html', browserReload);
+function img() {
+    return gulp.src(paths.images.src)
+    .pipe(newer(paths.images.dest))
+    .pipe(imagemin())
+    .pipe(gulp.dest(paths.images.dest))
+    .pipe(browsersync.stream())
 };
 
-exports.styles = styles;
-exports.sync = sync;
-exports.default = series(sync, watchFiles);
+function watch() {
+    browsersync.init({
+        server: {
+            baseDir: 'build/'
+        }
+    })
+    gulp.watch(paths.styles.src, styles)
+    gulp.watch(paths.html.src, copyHtml)
+    gulp.watch(paths.html.dest).on('change', browsersync.reload)
+    gulp.watch(paths.images.src, img)
+    gulp.watch(paths.images.src).on('unlink', cleanImg)
+    gulp.watch(paths.images.src).on('change', browsersync.reload)
+    gulp.watch(paths.fonts.src, copyFonts)
+    gulp.watch(paths.fonts.src).on('unlink', cleanFonts)
+    gulp.watch(paths.images.src).on('change', browsersync.reload)
+};
+
+
+const build = gulp.series(clean, copyHtml, copyFonts, img, styles, watch);
+
+exports.clean = clean;
+exports.build = build;
+exports.default = build;
