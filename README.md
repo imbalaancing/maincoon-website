567e83773f02a7e876f476d0012b0b4e2d20164f
nicky <nikkalaancing@gmail.com>
2022-08-21 23:22:28 +0700

    finishing touches

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # maincoon-website
+
+<img src="./img/readme-img.png" alt="maincoon-website">
+
+## Training project on layout of a static site about cats using the gulp task manager
+
+### to run the project locally, you need to use the default "gulp" command or the "build" command
--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const del = require('del');
 const cleanCSS = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
 const imagemin = require('gulp-imagemin');
-const browsersync = require('browser-sync').create();
+const browserSync = require('browser-sync').create();
 const newer = require('gulp-newer');
 
 const paths = {
@@ -27,31 +27,57 @@ const paths = {
     }
 };
 
-function clean() {
-    return del(['build/*', '!build/img']);
-};
-
+/**
+ * cleaning img build directory
+ */
 function cleanImg() {
     return del(['build/img/']);
 };
 
+/**
+ * cleaning fonts build directory
+ */
 function cleanFonts() {
     return del(['build/fonts/'])
 };
 
+/**
+ * removed build catalog
+ */
+function removeBuild() {
+    console.info('removed build catalog');
+
+    return del(['build/**/*'])
+};
+
+/**
+ * control and replacement of changed html
+ */
 function copyHtml() {
+    console.info('html file replaced');
+
     return gulp.src(paths.html.src)
     .pipe(gulp.dest(paths.html.dest))
-    .pipe(browsersync.stream())
+    .pipe(browserSync.stream())
 };
 
+/**
+ * control and replacement of changed fonts
+ */
 function copyFonts() {
+    console.info('all font files replaced');
+
     return gulp.src(paths.fonts.src)
     .pipe(gulp.dest(paths.fonts.dest))
-    .pipe(browsersync.stream())
+    .pipe(browserSync.stream())
 };
 
+/**
+ * assembly and control of style changes
+ */
 function styles() {
+    console.info('style file has been compiled');
+
     return gulp.src(paths.styles.src)
     .pipe(sourcemaps.init())
     .pipe(sass())
@@ -62,37 +88,67 @@ function styles() {
     }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.styles.dest))
-    .pipe(browsersync.stream())
+    .pipe(browserSync.stream())
 };
 
+/**
+ * assembly and preparation of images
+ */
 function img() {
+    console.info('img files for build prepared');
+
     return gulp.src(paths.images.src)
     .pipe(newer(paths.images.dest))
     .pipe(imagemin())
-    .pipe(gulp.dest(paths.images.dest))
-    .pipe(browsersync.stream())
+    .pipe(gulp.dest(paths.images.dest));
 };
 
-function watch() {
-    browsersync.init({
+/**
+ * image change control
+ */
+function imgStreamHandler() {
+    console.info('img files for stream prepared');
+
+    del(['build/img/']);
+
+    return gulp.src(paths.images.src)
+    .pipe(gulp.dest(paths.images.dest))
+    .pipe(browserSync.stream());
+};
+
+/**
+ * tracking changes in project files.
+ */
+function observer() {
+    console.info('observer has been started');
+    
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.html.src, copyHtml);
+    gulp.watch(paths.html.dest).on('change', browserSync.reload);
+    gulp.watch(paths.images.src, imgStreamHandler);
+    gulp.watch(paths.images.src).on('unlink', cleanImg);
+    gulp.watch(paths.images.src).on('change', browserSync.reload);
+    gulp.watch(paths.fonts.src, copyFonts);
+    gulp.watch(paths.fonts.src).on('unlink', cleanFonts);
+    gulp.watch(paths.images.src).on('change', browserSync.reload);
+};
+
+/**
+ * start and restart the project server
+ */
+function sync() {
+    console.info('browser sync function has been started');
+
+    browserSync.init({
         server: {
             baseDir: 'build/'
         }
-    })
-    gulp.watch(paths.styles.src, styles)
-    gulp.watch(paths.html.src, copyHtml)
-    gulp.watch(paths.html.dest).on('change', browsersync.reload)
-    gulp.watch(paths.images.src, img)
-    gulp.watch(paths.images.src).on('unlink', cleanImg)
-    gulp.watch(paths.images.src).on('change', browsersync.reload)
-    gulp.watch(paths.fonts.src, copyFonts)
-    gulp.watch(paths.fonts.src).on('unlink', cleanFonts)
-    gulp.watch(paths.images.src).on('change', browsersync.reload)
+    });
 };
 
+/**
+ * Assembly of the final project
+ */
+gulp.task('default', gulp.parallel(removeBuild, copyHtml, copyFonts, imgStreamHandler, styles, observer, sync));
+gulp.task('build', gulp.series(removeBuild, copyHtml, copyFonts, img, styles));
 
-const build = gulp.series(clean, copyHtml, copyFonts, img, styles, watch);
-
-exports.clean = clean;
-exports.build = build;
-exports.default = build;
